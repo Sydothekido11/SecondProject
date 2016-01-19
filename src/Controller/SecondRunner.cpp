16e99a168a2e4e1eada9e59dff6e31422a7b2222
Sydothekido11 <snem8901@740s-pl.local>
2016-01-19 13:51:24 -0700

    Made some changes in runner.

--- src/Controller/SecondRunner.cpp
+++ src/Controller/SecondRunner.cpp
@@ -4,7 +4,13 @@
  *  Created on: Jan 19, 2016
  *      Author: snem8901
  */
-
+#include "SecondController.h"
+int main()
+{
+	SecondController * baseController = new SecondController();
+	baseController->start();
+	return 0;
+}
 
 
 
