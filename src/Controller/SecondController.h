83f8c3776f620e2aae6e43efc9f9b24b119650b1
Sydothekido11 <snem8901@740s-pl.local>
2016-01-19 12:42:11 -0700

    Made a classCount

---
+++ src/Controller/SecondController.h
@@ -0,0 +1,24 @@
+/*
+ * SecondController.h
+ *
+ *  Created on: Jan 19, 2016
+ *      Author: snem8901
+ */
+
+#ifndef CONTROLLER_SECONDCONTROLLER_H_
+#define CONTROLLER_SECONDCONTROLLER_H_
+
+class AfternoonController
+{
+private:
+	int classCount;
+public:
+	SecondController();
+	void setClassCount();
+	int getClassCount();
+	void start();
+};
+
+
+
+#endif /* CONTROLLER_SECONDCONTROLLER_H_ */
